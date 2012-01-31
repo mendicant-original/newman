c2e923e8a1ccc48de4c85b7b7fc7e3e89d9eea6a
Gregory Brown <gregory.t.brown@gmail.com>
2012-01-31 18:20:17 -0500

    Various improvements to simple mailing list
    Added support for HTML emails, also added simple persistence of the list of
    subscribers. Made other miscellaneous improvements. This example is now ugly as
    hell, but will serve as a fine basis for improving the framework code.

--- examples/simple_mailing_list.rb
+++ examples/simple_mailing_list.rb
@@ -1,19 +1,28 @@
 require_relative "../lib/postal_service"
 require_relative "config"
+require "pstore"
 
-list_members = []
+
+store = PStore.new("simple_mailing_list.store")
+store.transaction { store["members"] ||= [] }
+
+def sender(mail)
+  mail.from.first.to_s
+end
 
 PostalService.run(CONFIGURATION_DATA) do |incoming, outgoing|
+  list_members = store.transaction(true) { store["members"] }
+
   if incoming.to.any? { |e| e[/\+subscribe@#{CONFIGURATION_DATA[:domain]}/] }
-    if list_members.include?(incoming.from)
+    if list_members.include?(sender(incoming))
       outgoing.subject = "ERROR: Already subscribed"
       outgoing.body    = "You are already subscribed, you can't subscribe again"
     else
       outgoing.subject = "SUBSCRIBED!"
-      list_members << incoming.from
+      store.transaction { store["members"] << sender(incoming) }
     end
    elsif incoming.to.any? { |e| e[/\+unsubscribe@#{CONFIGURATION_DATA[:domain]}/] }
-    if list_members.delete(incoming.from)
+    if store.transaction { store["members"].delete(sender(incoming)) }
       outgoing.subject = "UNSUBSCRIBED!"
       outgoing.body    = "Sorry to see you go!"
     else
@@ -21,9 +30,21 @@ PostalService.run(CONFIGURATION_DATA) do |incoming, outgoing|
       outgoing.body    = "You tried to unsubscribe, but you are not on our list!"
     end
   else
-    outgoing.reply_to = CONFIGURATION_DATA[:default_sender]
-    outgoing.bcc      = list_members.join(", ")
-    outgoing.subject  = incoming.subject
-    outgoing.body     = incoming.body.to_s
+    if list_members.include?(sender(incoming))
+      outgoing.from      = incoming.from
+      outgoing.reply_to  = CONFIGURATION_DATA[:default_sender]
+      outgoing.bcc       = list_members.join(", ")
+      outgoing.subject   = incoming.subject
+
+      if incoming.multipart?
+        outgoing.text_part = incoming.text_part
+        outgoing.html_part = incoming.html_part
+      else
+        outgoing.body = incoming.body.to_s
+      end
+    else
+      outgoing.subject = "You are not subscribed"
+      outgoing.body    = "You must be a member to post on this list"
+    end
   end
 end
