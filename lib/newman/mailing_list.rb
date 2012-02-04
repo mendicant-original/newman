928c1f14b88447fff8f6fc41d8ebffb172a370b9
Gregory Brown <gregory.t.brown@gmail.com>
2012-02-04 13:14:36 -0500

    Ensure that mailing list remains in a consistent state. Closes #2

--- lib/newman/mailing_list.rb
+++ lib/newman/mailing_list.rb
@@ -5,11 +5,22 @@ module Newman
       self.store = store
     end
 
+    # returns true if the subscription is new,
+    # false if the subscription already exists
     def subscribe(email)
+      return false if subscriber?(email)
+
       store[name].create(email)
+
+      true
     end
 
+    # returns true if the user is sucessfully unsubscribed,
+    # false if the email does not match anyone on the 
+    # subscriber list
     def unsubscribe(email)
+      return false unless subscriber?(email)
+
       record = store[name].find { |e| e.contents == email } 
       store[name].destroy(record.id)
     end
