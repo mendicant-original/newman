1bb75581b8c8c46f24069e48270706a16e6d10b2
Eric Gjertsen <ericgj72@gmail.com>
2012-02-05 21:43:35 -0500

    mailer that does not depend on singleton Mail config

--- lib/newman/mailer.rb
+++ lib/newman/mailer.rb
@@ -2,40 +2,59 @@ module Newman
   Mailer = Object.new
 
   class << Mailer
+    RETRIEVER_PROTOCOL = :imap
+    SENDER_PROTOCOL    = :smtp
+    
     def configure(settings)
       imap = settings.imap
       smtp = settings.smtp
 
-      Mail.defaults do
-         retriever_method :imap, 
+      @retriever_settings = {
            :address    => imap.address,
            :user_name  => imap.user,
            :password   => imap.password,
            :enable_ssl => imap.ssl_enabled || false,
            :port       => imap.port
-
-         delivery_method :smtp,
+      }
+      
+      @delivery_settings = {
            :address              => smtp.address,
            :user_name            => smtp.user,
            :password             => smtp.password,
            :authentication       => :plain,
            :enable_starttls_auto => smtp.starttls_enabled || false,
            :port                 => smtp.port
-      end
+      }
+      
+    end
+    
+    def messages
+      retriever.all(:delete_after_find => true)
+    end
 
-      def messages
-        Mail.all(:delete_after_find => true)
-      end
-
-      def new_message(*a, &b)
-        Mail.new(*a, &b)
-      end
-
-      def deliver_message(*a, &b)
-        new_message(*a, &b).deliver
+    def new_message(*a, &b)
+      Mail.new(*a, &b).tap do |msg| 
+        msg.delivery_method SENDER_PROTOCOL, delivery_settings
       end
     end
 
+    def deliver_message(*a, &b)
+      new_message(*a, &b).deliver
+    end
+
+    def retriever_class
+      @retriever_class ||= 
+        Mail::Configuration.instance.lookup_delivery_method(RETRIEVER_PROTOCOL)
+    end
+    
+    def retriever
+      retriever_class.new(retriever_settings)
+    end
+    
     attr_accessor :settings
+    
+    def delivery_settings;  @delivery_settings  ||= {}; end
+    def retriever_settings; @retriever_settings ||= {}; end
+    
   end
 end
