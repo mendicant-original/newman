15d18f38792852839c405b221457d113a36cd444
Gregory Brown <gregory.t.brown@gmail.com>
2012-02-08 11:21:41 -0500

    rename Newman::Store::Recorder to Newman::Recorder

---
+++ lib/newman/recorder.rb
@@ -0,0 +1,61 @@
+module Newman
+  Record = Struct.new(:column, :id, :contents)
+
+  class Recorder
+    include Enumerable
+
+    def initialize(column, store)
+      self.column = column
+      self.store  = store
+
+      store.write do |data|
+        data[:identifiers][column] ||= 0
+        data[:columns][column]     ||= {}
+      end
+    end
+
+    def each
+      store.read do |data|
+        data[:columns][column].each do |id, contents| 
+          yield(Record.new(column, id,contents)) 
+        end
+      end
+    end
+
+    def create(contents)
+      store.write do |data| 
+        id = (data[:identifiers][column] += 1)
+        
+        data[:columns][column][id] = contents 
+
+        Record.new(column, id, contents)
+      end
+    end
+
+    def read(id)
+      store.read do |data|
+        Record.new(column, id, data[:columns][column][id])
+      end
+    end
+
+    def update(id)
+      store.write do |data|
+        data[:columns][column][id] = yield(data[:columns][column][id])
+
+        Record.new(column, id, data[:columns][column][id])
+      end
+    end
+
+    def destroy(id)
+      store.write do |data|
+        data[:columns][column].delete(id)
+      end
+
+      true
+    end
+
+    private
+
+    attr_accessor :column, :store
+  end
+end
--- lib/newman/store.rb
+++ lib/newman/store.rb
@@ -1,7 +1,19 @@
-require_relative "store/recorder"
+# `Newman::Store` is a minimal persistence layer for storing non-relational
+# data. It is meant to make the task of building small applications with simple
+# data storage needs easier.
+#
+# For an example of how `Newman::Store` can be used in your applications, you
+# can take a look at how `Newman::MailingList` is implemented. A similar
+# approach could be used to develop arbitrary persistent models.
+#
+# `Newman::Store` is part of Newman's **external interface**.
 
 module Newman
   class Store 
+
+    # ---
+    
+    # TODO
     def initialize(filename)
       FileUtils.mkdir_p(File.dirname(filename))
 
@@ -13,22 +25,35 @@ module Newman
       end
     end
 
-    attr_reader :identifiers
-
+    # ---
+    
+    # TODO
     def [](column)
       Recorder.new(column, self) 
     end
 
+    # ---
+    
+    # TODO
     def read
       data.transaction(:read_only) { yield(data) }
     end
 
+    # ---
+    
+    # TODO
     def write
       data.transaction { yield(data) }
     end
 
+    # ---
+    
+    # TODO
     private
 
+    # ---
+    
+    # TODO
     attr_accessor :data
   end
 end
