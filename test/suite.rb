cfadb6601e173faf3177c15902ddcc11d2c67326
Gregory Brown <gregory.t.brown@gmail.com>
2012-02-11 22:30:37 -0500

    Add new test to suite

--- test/suite.rb
+++ test/suite.rb
@@ -4,3 +4,4 @@ SimpleCov.start
 require_relative "integration/acid_tests"
 require_relative "integration/skip_response_test"
 require_relative "integration/subject_filter_test"
+require_relative "integration/to_filter_test"
